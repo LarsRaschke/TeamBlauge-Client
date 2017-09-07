080b61e55c78212bc01052409b21d0b2afca01ea
AW <anke94.withake@googlemail.com>
2017-09-07 12:10:14 +0200

    Methode: ladeProjekte

--- src/model/interfaces/RMI_Projekt.java
+++ src/model/interfaces/RMI_Projekt.java
@@ -1,10 +1,10 @@
 package model.interfaces;
 
+import java.rmi.Remote;
+
 import model.User;
 
-public interface RMI_Projekt {
-	
-	public void userHinzufügen(User u);
+public interface RMI_Projekt extends Remote{
 	
 	public void taskHinzufügen(String name, String kommentar, User u);
 
