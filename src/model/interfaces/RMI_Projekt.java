e05c24d2b91e827eca134c33f8b20b4c87e864f2
AW <anke94.withake@googlemail.com>
2017-09-11 15:06:31 +0200

    Interfaces weitergeführt

--- src/model/interfaces/RMI_Projekt.java
+++ src/model/interfaces/RMI_Projekt.java
@@ -1,11 +1,20 @@
 package model.interfaces;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
 import model.User;
 
-public interface RMI_Projekt extends Remote{
+public interface RMI_Projekt extends Remote {
 	
-	public void taskHinzufügen(String name, String kommentar, User u);
+	public void taskHinzufügen(String name, String kommentar, User u) throws RemoteException;
 
+	public List<User> userListe() throws RemoteException;
+	
+	public List<String[]> taskListe() throws RemoteException;
+	
+	public String getProjektname() throws RemoteException;
+	
+	public String getBeschreibung() throws RemoteException;
 }
